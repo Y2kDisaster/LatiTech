0cb16efe6b3489530ce3f43c5119a79afb0400fa
Y2kDisaster <joelgonzalezgarcia6@gmail.com>
2024-11-06 00:07:44 -0600

    Se insertó un frame de Power BI

---
+++ reportes.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="icon" type="image/x-icon" href="img/latitude.ico">
+    <link rel="stylesheet" type="text/css" href="css/asignaciones2.css">
+    <title>Consulta de Asignaciones</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="js/asignaciones.js"></script>
+</head>
+<body>
+    <div class="header">
+        <div>
+        <a href="https://www.latitude.mx"><img class="logo" src="img/latitude2.png" alt="Logo de la empresa"></a>
+        </div>
+        <div class="menu-container">
+            <!-- Aquí puedes agregar los elementos de tu menú -->
+            <a class="button-menu" href="inicio.php">Inicio</a>
+            <a class="button-menu" href="asignaciones.php">Asignaciones</a>
+            <a class="button-menu" href="empleados.php">Empleados</a>
+            <a class="button-menu" href="equipos.php">Equipos</a>
+            <a class="button-menu logout" href="loging.php">Cerrar sesión</a>
+        </div>
+    </div>
+    <h1>Reportes</h1>
+    <center><iframe title="Reporte Equipos - LatiTech" width="1280" height="810" 
+    src="https://app.powerbi.com/view?r=eyJrIjoiNDgwODI2ZDctZjY1NC00YjgyLTkxYzEtMWJhZWNjMjRkOGIzIiwidCI6ImNhY2E5MDExLTdiNmEtNDRkZS04NjFmLTA5NWEyY2E4ODNiNyIsImMiOjR9"
+         frameborder="0" allowFullScreen="true"></iframe></center>
+</body>
+</html>

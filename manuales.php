7d97adebff982d16a47db7f2a0352ad7a6de8cb7
unknown <joelgonzalezgarcia6@gmail.com>
2024-11-21 15:08:39 -0600

    Se agregó un iframe del manual

---
+++ manuales.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="icon" type="image/x-icon" href="img/latitude.ico">
+    <link rel="stylesheet" type="text/css" href="css/consultas.css">
+    <title>Reportes</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="js/asignaciones.js"></script>
+</head>
+<body>
+    <div class="header">
+        <div>
+        <a href="https://www.latitude.mx"><img class="logo" src="img/latitude2.png" alt="Logo de la empresa"></a>
+        </div>
+        <div class="menu-container">
+            <!-- Aquí puedes agregar los elementos de tu menú -->
+            <a class="button-menu" href="inicio.php">Inicio</a>
+            <a class="button-menu" href="asignaciones.php">Asignaciones</a>
+            <a class="button-menu" href="empleados.php">Empleados</a>
+            <a class="button-menu" href="equipos.php">Equipos</a>
+            <a class="button-menu" href="organizacion.php">Organigrama</a>
+            <a class="button-menu" href="reportes.php">Reportes</a>
+            <a class="button-menu logout" href="loging.php">Cerrar sesión</a>
+        </div>
+    </div>
+    <h1>Reportes</h1>
+    <iframe src="https://scribehow.com/embed/Manual_LatiTech__wc-PMKbMRBuauIDUfI-qQA?as=scrollable"
+        width="1280" height="640" allowfullscreen frameborder="0"></iframe>
+</body>
+</html>
